408b1d78e4437c5e1ce466362b4e9c17b475549e
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-10-14 15:20:12 +0700

    fix: layout

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,7 +1,9 @@
 export default function CreateQuizPage() {
   return (
-    <div className="flex min-h-screen flex-col px-10 py-14">
-      <h1 className="text-lg font-bold">Create a New Quiz</h1>
+    <div className="flex min-h-screen flex-col px-10 py-14 md:px-32">
+      <div className="rounded-md border-2 p-4 shadow-sm">
+        <h1 className="text-lg font-bold">Create a New Quiz</h1>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col px-10 py-14">
-        <h1 className="text-lg font-bold">Home</h1>
+      <main className="flex min-h-screen flex-col px-10 py-14 md:px-32">
+        <div className="rounded-md border-2 p-4 shadow-sm">
+          <h1 className="text-lg font-bold">Home</h1>
+        </div>
       </main>
     </HydrateClient>
   );
