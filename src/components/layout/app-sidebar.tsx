ee7dfbab8885e8995e0cd31d34bffe2a16e3fad7
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-10-19 20:26:46 +0700

    fix: overide

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -3,19 +3,7 @@
 import * as React from "react";
 import { CirclePlus, Home, Save } from "lucide-react";
 import { NavMain } from "~/components/layout/nav-main";
-import {
-  SidebarMenu,
-  SidebarMenuItem,
-  SidebarMenuButton,
-} from "~/components/ui/sidebar";
-import { Button } from "~/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "~/components/ui/dropdown-menu";
-import { ChevronUp, CircleUser } from "lucide-react";
+import { NavUser } from "~/components/layout/nav-user";
 
 import {
   Sidebar,
@@ -24,8 +12,6 @@ import {
   SidebarRail,
 } from "~/components/ui/sidebar";
 import type { Session } from "next-auth";
-import Link from "next/link";
-import Image from "next/image";
 
 // This is sample data.
 const data = {
@@ -69,56 +55,15 @@ export function AppSidebar({
         <NavMain items={data.navMain} />
       </SidebarContent>
       <SidebarFooter>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuButton>
-                  <div className="flex items-center gap-2">
-                    <Image
-                      src={session?.user?.image ?? ""}
-                      alt="profile"
-                      className="h-[24px] w-[24px] overflow-hidden rounded-full"
-                      width={24}
-                      height={24}
-                    />
-                    <p className="text-[16px] font-semibold">
-                      {session?.user?.name}
-                    </p>
-                  </div>
-                  <ChevronUp className="ml-auto" />
-                </SidebarMenuButton>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                side="top"
-                className="flex w-[--radix-popper-anchor-width] flex-col gap-2 p-2"
-              >
-                <DropdownMenuItem>
-                  <Link href="/app/profile">
-                    <div className="flex items-center gap-2">
-                      <CircleUser />
-                      <p className="text-[16px] font-semibold">Profile</p>
-                    </div>
-                  </Link>
-                </DropdownMenuItem>
-
-                <Link
-                  href={
-                    session
-                      ? "/api/auth/signout?callbackUrl=/"
-                      : "/api/auth/signin"
-                  }
-                >
-                  <div>
-                    <Button className="w-full">
-                      {session ? "Sign Out" : "Sign In"}
-                    </Button>
-                  </div>
-                </Link>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </SidebarMenuItem>
-        </SidebarMenu>
+        {session?.user && (
+          <NavUser
+            user={{
+              name: session.user.name ?? "Unknown",
+              email: session.user.email ?? "Unknown",
+              image: session.user.image ?? "/fern.jpg",
+            }}
+          />
+        )}
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
