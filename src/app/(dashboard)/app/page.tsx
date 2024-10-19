b45bd2f879d6e03c3fa335a968698661059c5ac8
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-10-19 23:19:59 +0700

    fix: responsive card

--- src/app/(dashboard)/app/page.tsx
+++ src/app/(dashboard)/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col gap-6 px-10 py-14 md:px-24">
       <div className="flex flex-col gap-4">
         <h1 className="text-xl font-bold">Overview</h1>
-        <div className="flex h-max flex-col gap-6 lg:flex-row">
+        <div className="flex h-max w-full flex-col justify-between gap-6 lg:flex-row">
           <QuizCard />
           <QuizBarChart />
           <QuizPieChart />
