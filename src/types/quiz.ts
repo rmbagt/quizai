1ad378a35e32eef8c9e15dec4a29282148850b63
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-10-20 15:10:30 +0700

    feat language

--- src/types/quiz.ts
+++ src/types/quiz.ts
@@ -1,12 +1,12 @@
 export interface QuizQuestion {
-    question: string;
-    choices: string[];
-    answer: number;
+  question: string;
+  choices: string[];
+  answer: number;
 }
 
 export interface QuizData {
-    theme: string;
-    questions: QuizQuestion[];
-    workingTime: number;
+  theme: string;
+  questions: QuizQuestion[];
+  workingTime: number;
+  language: string;
 }
-
